26b6c165d74ef1c4906acfeb4cf2cd989162dc48
Dustin Dev <94726570+dustinFriesenDev@users.noreply.github.com>
2024-05-21 10:09:36 -0400

    first commit

---
+++ src/test/java/com/cavalcade/queue/QueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cavalcade.queue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
